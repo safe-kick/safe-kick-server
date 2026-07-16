896acbf8a733410910a5bb4b468c3a81639e422f
jungjayyoung <jaywjd@gmail.com>
2026-07-16 16:42:32 +0900

    Mock 인증 제거 및 DB 기반 회원가입 로그인 연동

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,7 @@
-const useMock = process.env.USE_MOCK === "false";
+const useMock = process.env.USE_MOCK === "true";
+
+console.log("USE_MOCK =", process.env.USE_MOCK);
+console.log("Controller =", useMock ? "Mock" : "DB");
 
 module.exports = useMock
   ? require("./authMockController")
